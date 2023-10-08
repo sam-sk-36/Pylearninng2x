b1fedbee238cb7536117b6a7a36b02e45c842b25
sam-sk-36 <147340065+sam-sk-36@users.noreply.github.com>
2023-10-08 23:49:30 +0530

    Add files via upload

---
+++ sam1.py
@@ -0,0 +1,30 @@
+n=input('Enter number\n')
+n=int(n)
+print(f'Table of {n} is :')
+
+print(f'{n} X 1 = {n*1}')
+print(f"{n} X 2 = {n*2}")
+print(f"{n} X 3 = {n*3}")
+print(f"{n} X 4 = {n*4}")
+print(f"{n} X 5 = {n*5}")
+print(f"{n} X 6 = {n*6}")
+print(f"{n} X 7 = {n*7}")
+print(f"{n} X 8 = {n*8}")
+print(f"{n} X 9 = {n*9}")
+print(f"{n} X 10 = {n*10}")
+
+print("String functions in python!")
+
+a='I am Sameer'
+
+print(len(a))
+
+print(a.upper())
+
+print(a.lower())
+
+print(a.strip())
+
+print(a.replace("S","J"))
+
+print(a.split(" "))
