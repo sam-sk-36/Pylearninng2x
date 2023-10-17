229ac2fd9d33cde58cad1b35e7305d99a796d26b
sam-sk-36 <147340065+sam-sk-36@users.noreply.github.com>
2023-10-17 10:21:13 +0530

    Add files via upload

---
+++ 13 OCT.py
@@ -0,0 +1,18 @@
+# task 1
+pi=3.14
+r=float(input(" enter the radius  "))
+area= pi * r**2
+print(f"Area of circle is {area}")
+
+# task 2
+
+a=int(input())
+b=int(input())
+c=int(input())
+
+if a>b and a>c :
+    print("A is large number")
+elif  b>c:
+    print("B is large number")
+else:
+    print(" C is large number.")
